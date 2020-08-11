ad5bf89792ab51b33db49a870c5f9073387a4f5c
Ranielison <ranielisonsoares2@gmail.com>
2020-08-10 23:09:17 -0300

    Extraindo componente de grid

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
+        debugShowCheckedModeBanner: false,
         home: ProductOverviewScreen(),
         routes: {
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/providers/products.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/widgets/grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: products.length,
-        itemBuilder: (ctx, i) => ProductItem(
-          product: products[i],
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
