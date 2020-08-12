d6de4e69ad0e68e55bd62b3fcae275d7623f074f
Ranielison <ranielisonsoares2@gmail.com>
2020-08-12 00:48:38 -0300

    Criando provider para carrinho

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -46,12 +46,13 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
             ],
           ),
           Consumer<Cart>(
-            builder: (ctx, cart, _) => Badge(
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+            ),
+            builder: (ctx, cart, child) => Badge(
               value: cart.itemCount.toString(),
-              child: IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-              ),
+              child: child,
             ),
           )
         ],
