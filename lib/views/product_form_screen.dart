add3bece3eaba519c72b2915fb25bc0bed9f934a
Ranielison <ranielisonsoares2@gmail.com>
2020-11-03 00:20:08 -0300

    feat: controlando o foco

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -6,11 +6,41 @@ class ProductFormScreen extends StatefulWidget {
 }
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
+  final _focusPreco = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_focusPreco);
+                },
+              ),
+              TextFormField(
+                focusNode: _focusPreco,
+                decoration: InputDecoration(
+                  labelText: 'Preço',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                textInputAction: TextInputAction.next,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
